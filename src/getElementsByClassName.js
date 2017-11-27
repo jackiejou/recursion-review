4782aacf22a89ed9ad3d8ac8fc0eded05eea2515
jackiejou <joujackie@gmail.com>
2017-11-27 15:38:20 -0800

    Completed Get elements by class and Stringify JSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  // loop through each element of the document.body for className
+  function searchElements (node) {
+    // check the element's classList if it contains className
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        // if yes, add to the results list
+        result.push(node);
+      }
+    }
+    // check if this element has childNodes
+    if (node.childNodes.length > 0) {
+      // if childNodes, then recall this function recursively
+      node.childNodes.forEach(function(item) {
+        searchElements(item);
+      });
+    }  
+  }
+  searchElements(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  // if obj is null, stringify null
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  // if obj is an Array, stringify each element
+  if (Array.isArray(obj)) {
+    var result = [];
+    if (obj.length > 0) {
+      for (var i = 0; i < obj.length; i++) {
+        result.push(stringifyJSON(obj[i]));
+      }
+      return '[' + result.join(',') + ']';
+    } else {
+      return '[]';
+    }
+    
+  }
+  // if obj is an Object, stringify each key and value pair
+
+  if (typeof obj === 'object') {
+    var result = [];
+    
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+      
+    return '{' + result.join(',') + '}';
+  }  
 };
